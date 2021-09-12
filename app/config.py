5091b315c388b0714ca8895d195822f0e2999beb
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-12 21:28:23 +0300

    Add articles api base url

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config:
     Parent configuration class with general configuration settings
     '''
     NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
     pass
 
