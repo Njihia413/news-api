a7e070b0347ecd10b72242c47c5cc6f083d2c2d4
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-12 17:05:24 +0300

    Add news api base url

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+from instance.config import NEWS_API_KEY
 from re import DEBUG
 
 
@@ -5,6 +6,8 @@ class Config:
     '''
     Parent configuration class with general configuration settings
     '''
+    NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+
     pass
 
 class ProdConfig(Config):
