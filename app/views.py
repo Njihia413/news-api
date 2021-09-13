c698357a6805a10af59eba90ca28dc883db396d5
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-13 10:45:01 +0300

    Add view functions for business,sports,entertainment,technology,health and science

--- app/views.py
+++ app/views.py
@@ -41,3 +41,80 @@ def articles(source_id):
         'articles.html',
         title = title,
         articles = articles)
+
+@app.route('/business')
+def business():
+    '''
+    View business page function that returns the business page and its data
+    '''
+    business = get_articles_depending_on_category('business')
+    title = 'Business - Welcome to the best News Resource Website Online'
+    return render_template('business.html',
+                           title=title,
+                           business=business
+                           )
+
+
+@app.route('/sports')
+def sports():
+    '''
+    View sports page function that returns the sports page and its data
+    '''
+    sports = get_articles_depending_on_category('sports')
+    title = 'Sports - Welcome to the best News Resource Website Online'
+    return render_template('sports.html',
+                           title=title,
+                           sports=sports
+                           )
+
+
+@app.route('/entertainment')
+def entertainment():
+    '''
+    View entertainment page function that returns the entertainment page and its data
+    '''
+    entertainment = get_articles_depending_on_category('entertainment')
+    title = 'Entertainment - Welcome to the best News Resource Website Online'
+    return render_template('entertainment.html',
+                           title=title,
+                           entertainment=entertainment
+                           )
+
+
+@app.route('/technology')
+def technology():
+    '''
+    View technology page function that returns the technology page and its data
+    '''
+    technology = get_articles_depending_on_category('technology')
+    title = 'Technology - Welcome to the best News Resource Website Online'
+    return render_template('technology.html',
+                           title=title,
+                           technology=technology
+                           )
+
+
+@app.route('/health')
+def health():
+    '''
+    View health page function that returns the health page and its data
+    '''
+    health = get_articles_depending_on_category('health')
+    title = 'Health - Welcome to the best News Resource Website Online'
+    return render_template('health.html',
+                           title=title,
+                           health=health
+                           )
+
+
+@app.route('/science')
+def science():
+    '''
+    View science page function that returns the science page and its data
+    '''
+    science = get_articles_depending_on_category('science')
+    title = 'Science - Welcome to the best News Resource Website Online'
+    return render_template('science.html',
+                           title=title,
+                           science=science
+                           )
